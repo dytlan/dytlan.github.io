9e5233546887fffeca16bc1debb7bf635e9efc51
dytlan <dytlanian@gmail.com>
2020-06-08 13:03:12 +0700

    Tambah dokumentasi

--- LatihanMFR/script.js
+++ LatihanMFR/script.js
@@ -9,26 +9,33 @@
  *  7. Simpan di dalam DOM
  **/
 
+ //Point 1
  const videos = Array.from(document.querySelectorAll('ul li'));
  
+ //Point 2
 const totalDuration = videos.filter(video => video.textContent.includes('JAVASCRIPT LANJUTAN'))
 
+//Point 3
 .map(video=>video.getAttribute('data-duration'))
 
+//Point 4
 .map(duration=>{
     let time = duration.split(':');
     return parseInt(time[0])*60 +parseInt(time[1]);
 })
 
+//Point 5
 .reduce((duration,current) => duration + current )
 
+//Point 6
 let hour    = Math.floor(totalDuration / 3600);
 let minute  = Math.floor((totalDuration - (3600 * hour))/60);
 let second  = Math.floor((totalDuration - (3600 * hour) - (minute * 60)))
 
+//Point 2
 const filterVIdeo = videos.filter(video => video.textContent.includes('JAVASCRIPT LANJUTAN'));
 
-
+//Point 7
 const textDuration = hour+' jam '+minute+' menit '+second+' detik';
 
 timeHTML = document.querySelector('.total-durasi');
